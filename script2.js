e6918722c0225de6400dcc5489d6d68f1b21bb0f
nathanc8 <cazard.nathan@gmail.com>
2024-07-12 11:48:46 +0200

    Exo fait en typescript

--- script2.js
+++ script2.js
@@ -1,5 +1,13 @@
-var message = "Bonjour !";
-console.log(message);
-var firstName = "Beyoncé";
-message = message.substring(0, message.length - 1) + firstName + " !";
-console.log(message);
+function sayHello(message) {
+    var firstName = prompt("Quel est ton prénom ?");
+    var hour = new Date().getHours();
+    if (hour > 5 || hour < 18) {
+        message = "Bonjour !";
+    }
+    else {
+        message = "Bonsoir !";
+    }
+    message = message.substring(0, message.length - 1) + firstName + " !";
+    document.querySelector("h1").innerText = message;
+}
+sayHello("");
--- script2.ts
+++ script2.ts
@@ -1,6 +1,13 @@
-let message: string = "Bonjour !";
-console.log(message);
-let firstName: string = "Beyoncé";
+function sayHello(message: string) {
+    const firstName: string | null = prompt("Quel est ton prénom ?");
+    const hour = new Date().getHours();
+    if (hour > 5 || hour < 18) {
+        message = "Bonjour !";
+    } else {
+        message = "Bonsoir !";
+    }
+    message = message.substring(0, message.length - 1) + firstName + " !";
+    document.querySelector("h1")!.innerText = message;
+}
 
-message = message.substring(0, message.length - 1) + firstName + " !";
-console.log(message);
+sayHello("");
