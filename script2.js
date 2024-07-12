a05907abbc868e7f3b55ebebb081459a456a36cf
nathanc8 <cazard.nathan@gmail.com>
2024-07-12 10:54:21 +0200

    oui

--- script2.js
+++ script2.js
@@ -1,6 +1,5 @@
-let message: string = "Bonjour !";
+var message = "Bonjour !";
 console.log(message);
-let firstName: string = "Beyoncé";
-
+var firstName = "Beyoncé";
 message = message.substring(0, message.length - 1) + firstName + " !";
 console.log(message);
